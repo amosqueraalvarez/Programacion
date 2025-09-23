d5b6233fb8defd4e28f1779ff017b64c0540f44d
Adrian <amosqueraalvarez@danielcastelao.org>
2025-09-23 15:13:55 +0200

    esto es solo un cambio

--- Boletines/boletin2.py
+++ Boletines/boletin2.py
@@ -16,3 +16,5 @@ print(areaCuadrado)
 euro = int(input('Ingrese la cantidad que desea cambiar: '))
 cambio = euro * 1.18
 print('Su saldo en dolares es: ' + str(cambio))
+
+# acabo de hacer un cambio
