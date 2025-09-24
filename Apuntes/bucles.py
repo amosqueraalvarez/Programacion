9303e29e4b16b54d3393432f2e243de5b312be61
Adrian <amosqueraalvarez@danielcastelao.org>
2025-09-24 13:31:59 +0200

    añadido apuntes

--- Apuntes/bucles.py
+++ Apuntes/bucles.py
@@ -1,4 +1,4 @@
-#bucles
+# bucles
 '''
 temp= 13
 while temp<100:
@@ -13,16 +13,61 @@ while contador<20:
     print(contador)
     contador = contador + 1
     
-'''
+
 # break y continue
-contador= 0
+contador = 0
 while True:
-    contador= contador +1
-    if contador>20:
+    contador = contador + 1
+    if contador > 20:
         break
-    if contador%3 != 0:
+    if contador % 3 != 0:
         continue
     print(contador)
 
 
+# bucle while para recorrer una lista
 
+t = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+
+i = 0
+suma = 0
+while i <= 7:
+    suma = suma + t[i]
+    i += 1  # incremento de 1 en 1
+    print(suma)
+
+# while se usa para los determinados
+# for se usa para los indeterminados
+'''
+'''
+t = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+suma = 0
+
+for elemento in t:  # elemento la acabo de crear y solo se puede usar dentro del for
+    print(elemento)
+    suma = suma + elemento
+print(suma)
+
+# defino la funcion len en el tamaño de la tupla
+suma = 0
+i = 0
+while i < len(t):  # si no quiero definir el tamaño concreto de la tupla y simplemento uso len para ver la longitud de la tupla
+    suma = suma + t[i]
+    i += 1  # incremento de 1 en 1
+    print(suma)
+
+for i in range(0, 8):  # recorre por el rango que nosotros definamos
+    print(i)
+    print(t[i])
+
+for i in range(7, 0, -1):  # hace una secuencia hacia atras
+    print(i)
+    print(t[i])
+'''
+
+t = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+
+# ojo estoy declarando dos variables dentro y la i me recoge una cosa y numero otra
+for i, numero in enumerate(t):
+    print(i)  # me enumera los elementos de la tupla
+    print(numero)  # me da el valor del elemento correspondiente de la tupla
--- Apuntes/listas_tuplas.py
+++ Apuntes/listas_tuplas.py
@@ -1,18 +1,18 @@
-#lista y tupla
+# lista y tupla
 
-#en lista necesitamos marcas la lista entre []
-l=[10, 20, 30, 40, 50]
-#como acceder a los datos:
+# en lista necesitamos marcas la lista entre []
+l = [10, 20, 30, 40, 50]
+# como acceder a los datos:
 # los indices (orden) empiezas desde 0 de izq a derecha
-#de derecha a izquierda van de -1 a x
+# de derecha a izquierda van de -1 a x
 
-#solo se separan por comas y van entre parentesis(no necesarias)
-t= (10, 20, 30, 40, 50)
+# solo se separan por comas y van entre parentesis(no necesarias)
+t = (10, 20, 30, 40, 50)
 
-l=[10, 20, 30, 40, 50]
-t= (10, 20, 30, 40, 50)
+l = [10, 20, 30, 40, 50]
+t = (10, 20, 30, 40, 50)
 
-#asi llamo al indice numero 2 de la lista
+# asi llamo al indice numero 2 de la lista
 print(l[2])
 # en las tuplas se llama igual entre []
 print(t[1])
@@ -21,15 +21,20 @@ print(l[-3])
 
 # una lista puede ser compuesta y contener una lista dentro de ella
 
-l=[10, 20, ['XXX', 'Treinta', 30],30, 40, 50]
-#busco dentro de la lista que hay en la lista
+l = [10, 20, ['XXX', 'Treinta', 30], 30, 40, 50]
+# busco dentro de la lista que hay en la lista
 print(l[2][1])
 
-#si quiero extraer solo una parte de la lista o de la tupla
-#utilizo los : entre inicio y final 1:3 coge el indice 1 y 2, el 3 no lo coge(en su defecto seria 1:4 para incluir el indice 3)
+# si quiero extraer solo una parte de la lista o de la tupla
+# utilizo los : entre inicio y final 1:3 coge el indice 1 y 2, el 3 no lo coge(en su defecto seria 1:4 para incluir el indice 3)
 
 print(t[1:3])
 
-t=(10,20,30,40,50,60,70,80,90,100)
-#asi marco inicio y final de lo que busca y que saque los pares
+t = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+# asi marco inicio y final de lo que busca y que saque los pares
 print(t[1:10:2])
+# recorre la lista hacia atras
+print(t[::-1])
+
+# las tuplas son inmutables
+# las listas si se pueden cambiar
