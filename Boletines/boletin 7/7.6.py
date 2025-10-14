1efc3d00a8a9c3a9be333aecde6b52393389cd3c
Adrian <amosqueraalvarez@danielcastelao.org>
2025-10-14 09:05:12 +0200

    7.6 y 7.7 añadidos

---
+++ Boletines/boletin 7/7.6.py
@@ -0,0 +1,6 @@
+# 6. Divide a cadea de texto “ www. phytonparatodos. com” en duas partes “ www. phyton” e “paratodos. com”.
+# Para posteriormente concaténalas e mostralas de novo.
+
+cadea_texto = 'www.phytonparatodos. com'
+print(cadea_texto[0:10])
+print(cadea_texto[10:])
---
+++ Boletines/boletin 7/7.7.py
@@ -0,0 +1,6 @@
+# Transforma a cadea de texto “ Phytoneros” a maiúsculas. Garda o valor na variable e
+# posteriormente convértea novamente a minúsculas.
+
+texto_1 = 'Phytoneros'
+texto_2 = texto_1.capitalize
+print(texto_2)
